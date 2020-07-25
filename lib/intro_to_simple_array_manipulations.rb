fa5d1f316f07ebdc0956aa6e3435fd57b23fc25f
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-25 03:19:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
-def doing_first_test(array, string)
+def using_push(array, string)
   array.push(string)
 end
 
-def the_second_test(array, string)
+def using_unshift(array, string)
   array.unshift(string)
 end
